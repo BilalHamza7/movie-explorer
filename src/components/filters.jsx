10e9c7ccfc4fdcb67e8c96773883fafea10ccfb3
Bilal Hamza <131806480+BilalHamza7@users.noreply.github.com>
2025-05-09 02:39:45 +0530

    added card grids and data fetching

--- src/components/filters.jsx
+++ src/components/filters.jsx
@@ -1,11 +1,11 @@
 import { Autocomplete, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import React, { useState } from 'react';
 
-const Filter = () => {
+const Filter = ({ genres, selectedGenre, handleSelectedGenre }) => {
 
   // search bar
   // search by genre, year and rating (vote_average)
-  const [genre, setGenre] = useState('');
+  // const [genre, setGenre] = useState('');
   const [year, setYear] = useState('');
   const [rating, setRating] = useState('');
 
@@ -15,25 +15,32 @@ const Filter = () => {
         id="search-movies"
         freeSolo
         // options={top100Films.map((option) => option.title)}
-        options={[1, 2, 3, 4, 54, 'bilal']}
+        options={['1', '2', '3', '4', '54', 'bilal']}
         renderInput={(params) => <TextField {...params} label="Search Movies" />}
       />
       <div className="flex gap-5 mt-6">
+
+        {/* Genre filter */}
         <FormControl fullWidth >
           <InputLabel id="genre-label">Genre</InputLabel>
           <Select
             labelId="genre-label"
-            value={genre}
+            value={selectedGenre}
             label="Genre"
-            onChange={(e) => setGenre(e.target.value)}
+            onChange={(e) => handleSelectedGenre(e.target.value)}
           >
-            <MenuItem value="Drama">Drama</MenuItem>
-            <MenuItem value="Action">Action</MenuItem>
-            <MenuItem value="Romance">Romance</MenuItem>
+            <MenuItem value=''><em>All</em></MenuItem>
+            {
+              genres?.map((item) =>
+                <MenuItem key={item.id} value={item.name}>
+                  {item.name}
+                </MenuItem>
+              )
+            }
           </Select>
         </FormControl>
 
-        {/* Year Select */}
+        {/* Year filter */}
         <FormControl fullWidth >
           <InputLabel id="year-label">Year</InputLabel>
           <Select
@@ -42,13 +49,22 @@ const Filter = () => {
             label="Year"
             onChange={(e) => setYear(e.target.value)}
           >
-            <MenuItem value="2023">2023</MenuItem>
-            <MenuItem value="2022">2022</MenuItem>
-            <MenuItem value="2021">2021</MenuItem>
+            <MenuItem value="0"><em>All</em></MenuItem>
+            <MenuItem value="2025">2025</MenuItem>
+            <MenuItem value="2024">2024</MenuItem>
+            <MenuItem value="2020-2025">2020-now</MenuItem>
+            <MenuItem value="2010-2025">2010-now</MenuItem>
+            <MenuItem value="2010-2019">2010-2019</MenuItem>
+            <MenuItem value="2000-2009">2000-2009</MenuItem>
+            <MenuItem value="1990-1999">1990-1999</MenuItem>
+            <MenuItem value="1980-1989">1980-1989</MenuItem>
+            <MenuItem value="1970-1979">1970-1979</MenuItem>
+            <MenuItem value="1950-1969">1950-1969</MenuItem>
+            <MenuItem value="1900-1949">1900-1949</MenuItem>
           </Select>
         </FormControl>
 
-        {/* Rating Select */}
+        {/* Rating filter */}
         <FormControl fullWidth >
           <InputLabel id="rating-label">Rating</InputLabel>
           <Select
@@ -57,9 +73,16 @@ const Filter = () => {
             label="Rating"
             onChange={(e) => setRating(e.target.value)}
           >
-            <MenuItem value="PG">PG</MenuItem>
-            <MenuItem value="PG-13">PG-13</MenuItem>
-            <MenuItem value="R">R</MenuItem>
+            <MenuItem value=""><em>All</em></MenuItem>
+            <MenuItem value="9+">9+</MenuItem>
+            <MenuItem value="8+">8+</MenuItem>
+            <MenuItem value="7+">7+</MenuItem>
+            <MenuItem value="6+">6+</MenuItem>
+            <MenuItem value="5+">5+</MenuItem>
+            <MenuItem value="4+">4+</MenuItem>
+            <MenuItem value="3+">3+</MenuItem>
+            <MenuItem value="2+">2+</MenuItem>
+            <MenuItem value="1+">1+</MenuItem>
           </Select>
         </FormControl>
       </div>
--- src/components/movieGrid.jsx
+++ src/components/movieGrid.jsx
@@ -1,31 +1,105 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import getAllMovies from '../api/getAllMovies';
+import getGenres from '../api/getGenres';
 import Filter from './filters'
-import { Grid } from '@mui/material';
+import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from '@mui/material';
 
 const MovieGrid = () => {
 
     // initialize filters and pass to the api
 
-    const getMovies = async () => {
-        const response = getAllMovies();
-        console.log(response);
-    };
+    // get all movies and display using state
+
+    // change in filter updates the movie state to be displayed
+
+    // count page number as a state and increment number before calling page 2 of the movies and extend
+    const [allMovies, setAllMovies] = useState([]);
+    const [pageNumber, setPageNumber] = useState(0);
+    const [genreList, setGenreList] = useState([]);
+    const [selectedGenre, setSelectedGenre] = useState('');
+
+    useEffect(() => {
+        const getMovies = async () => {
+            const response = await getAllMovies();
+            console.log(response);
+            setAllMovies(response.results);
+            setPageNumber(response.page);
+        };
+
+        const getAllGenres = async () => {
+            const response = await getGenres();
+            console.log(response);
+            setGenreList(response.genres);
+        }
+
+        getAllGenres();
+        getMovies();
+    }, []);
 
     return (
         <div className="flex flex-col items-center min-h-screen p-6 bg-[#CCC9DC]">
-            <Filter />
-            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} sx={{ width: '100%' }}>
-                {Array.from(Array(6)).map((_, index) => (
-                    <Grid key={index} size={{ xs: 2, sm: 4, md: 4 }}>
-                        <div className='bg-gray-300 w-full'>{index + 1}</div>
+            <Filter genres={genreList} selectedGenre={selectedGenre} handleSelectedGenre={(value) => setSelectedGenre(value)} />
+            <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 9, md: 16 }} sx={{ width: '100%', marginTop: 4 }}>
+                {allMovies.map((movie, index) => (
+                    <Grid key={index} size={{ xs: 2, sm: 3, md: 4 }}>
+                        <Card sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+                            <CardActionArea sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+                                <CardMedia
+                                    component="img"
+                                    height="160"
+                                    image={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+                                    alt={movie.original_title}
+                                    sx={{ objectFit: 'cover', width: '100%' }}
+                                />
+                                <CardContent sx={{ flexGrow: 1, p: 2, width: '100%' }}>
+
+                                    {/* Movie Title */}
+                                    <Typography
+                                        variant="h6"
+                                        component="div"
+                                        sx={{ fontSize: { xs: 16, md: 20 }, fontWeight: 600 }}
+                                    >
+                                        {movie.original_title}
+                                    </Typography>
+
+                                    {/* Movie Release Year and Rating */}
+                                    <Typography
+                                        variant="h6"
+                                        component="div"
+                                        sx={{ fontSize: { xs: 14, md: 16 }, fontWeight: 400 }}
+                                    >
+                                        {new Date(movie.release_date).getFullYear()}
+                                    </Typography>
+                                    <Typography
+                                        variant="h6"
+                                        component="div"
+                                        sx={{ fontSize: { xs: 14, md: 16 }, fontWeight: 400 }}
+                                    >
+                                        {movie.vote_average.toFixed(1)}
+                                    </Typography>
+
+                                    {/* <Typography
+                                        variant="body2"
+                                        sx={{
+                                            color: 'text.secondary',
+                                            fontSize: { xs: 12, md: 14 },
+                                            overflow: 'hidden',
+                                            textOverflow: 'ellipsis',
+                                            display: '-webkit-box',
+                                            WebkitLineClamp: 3,
+                                            WebkitBoxOrient: 'vertical',
+                                        }}
+                                    >
+                                        {movie.overview}
+                                    </Typography> */}
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
                     </Grid>
                 ))}
             </Grid>
             <p>Movies</p>
-            <button onClick={() => getMovies()}>Get Movies
-            </button>
-        </div>
+        </div >
     )
 }
 
